68cf025f612ce38ee13b3d9018ce73ebd6841052
Robert Eschbach <email@robert-eschbach.de>
2025-01-13 17:42:43 +0100

    change / adapt path and cmucl specific parameters

--- art/source/lisp-init.lisp
+++ art/source/lisp-init.lisp
@@ -1,18 +1,14 @@
-(in-package 'common-lisp-user)
+(in-package "COMMON-LISP-USER")
 
 (defun load-art ()
-  (load "/home/systeme/art/source/loaders/art-loader"))
+  (load "/media/sf_Shared-Folder/kem-systems/art/source/loaders/art-loader"))
 
 (format t "type '(load-art)' to load ART~%")
 
-#+:cmu 
+;#+:cmu
 (progn 
-  (defparameter *load-binary-pathname-types* *load-object-types*)
-  (defparameter *load-source-pathname-types* *load-source-types*)
+  ;(defparameter *load-binary-pathname-types* *load-object-types*)
+  ;(defparameter *load-source-pathname-types* *load-source-types*)
+  (defparameter *load-binary-pathname-types* '("fasl" "x86f" "sparcf" "ppcf" "mipsf" "alpha"))
+  (defparameter *load-source-pathname-types* '("fasl" "lisp" "cl" "lsp"))
   (export '(*load-binary-pathname-types* *load-source-pathname-types*)))
-
-
-
-
-
-
